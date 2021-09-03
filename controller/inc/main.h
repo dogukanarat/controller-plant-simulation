1b6503b30f931b194be0ddad4598e54d01369bea
dogukanarat <aratdogukan@gmail.com>
2021-09-03 20:47:06 +0300

    Cpp style OSAL is tested

--- controller/inc/main.h
+++ controller/inc/main.h
@@ -1,12 +1,8 @@
 #ifndef _CONTROLLER_MAIN_H
 #define _CONTROLLER_MAIN_H
 
-#include "os_stream.h"
-#include "os_thread.h"
-#include "os_timer.h"
-#include "os_memory.h"
 #include "os_socket.h"
-#include "os_stream.h"
+#include "osal.h"
 #include <Eigen/Dense>
 #include "frame.h"
 #include "generic.h"
--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -1,5 +1,7 @@
 #include "main.h"
 
+using namespace OSAL;
+
 void *server( void *arg )
 {
     UNUSED( arg );
@@ -8,46 +10,46 @@ void *server( void *arg )
     Needmon::Frame messageFrame;
     Packets::Periodic periodicPacket;
 
-    OS_display("[SERVER] Controller server has been started! ");
+    OS::display("[SERVER] Controller server has been started! ");
 
     const uint16_t port_number_u16     = 5001;
-    int            server_socket_int   = OS_socket_tcp();
+    int            server_socket_int   = Socket::_socketStream();
     int            client_socket_int   = 0;
 
-    OS_socket_address_in_t *p_server_socket_address_in_st = OS_socket_address_in_init( port_number_u16 );
-    OS_socket_address_in_t *p_client_socket_address_in_st = (OS_socket_address_in_t*)OS_mem_allocate( sizeof(OS_socket_address_in_t) );
+    Socket::address_in_t* p_server_socket_address_in_st = Socket::address_in_init( port_number_u16 );
+    Socket::address_in_t* p_client_socket_address_in_st = (Socket::address_in_t*)OS::memAllocate( sizeof(Socket::address_in_t) );
 
-    OS_socket_length_t server_socklen_st = sizeof(*p_server_socket_address_in_st);
-    OS_socket_length_t client_socklen_st = sizeof(*p_client_socket_address_in_st);
+    Socket::length_t server_socklen_st = sizeof(*p_server_socket_address_in_st);
+    Socket::length_t client_socklen_st = sizeof(*p_client_socket_address_in_st);
 
     
-    if( OS_bind( server_socket_int, (OS_socket_address_t*)p_server_socket_address_in_st, server_socklen_st) < 0 )
+    if( Socket::_bind( server_socket_int, (Socket::address_t*)p_server_socket_address_in_st, server_socklen_st) < 0 )
     {
-        OS_display("[ERROR] Bind error!");
+        OS::display("[ERROR] Bind error!");
     }
 
-    if( OS_listen( server_socket_int) < 0 )
+    if( Socket::_listen( server_socket_int) < 0 )
     {
-        OS_display("[ERROR] Listen error!");
+        OS::display("[ERROR] Listen error!");
     }
 
     while( true )
     {
-        OS_display("[SERVER] Server in the loop!");
+        OS::display("[SERVER] Server in the loop!");
 
-        client_socket_int = OS_accept( server_socket_int, (OS_socket_address_t*)p_client_socket_address_in_st, &client_socklen_st );
+        client_socket_int = Socket::_accept( server_socket_int, (Socket::address_t*)p_client_socket_address_in_st, &client_socklen_st );
 
-        OS_read( client_socket_int, messageBuffer.GetBuffer(), Needmon::FRAME_SIZE);
+        Socket::_read( client_socket_int, messageBuffer.GetBuffer(), Needmon::FRAME_SIZE);
 
         messageFrame.Parse( messageBuffer );
         periodicPacket.Decode( messageFrame );
 
-        OS_print("[CLIENT] Received message: 0x%X \n", periodicPacket.Data.data1);
+        OS::print("[CLIENT] Received message: 0x%X \n", periodicPacket.Data.data1);
         
 
-        OS_close( client_socket_int );
+        Socket::_close( client_socket_int );
 
-        OS_wait_us(1000000);
+        OS::waitUs(1000000);
 
     }
     
@@ -66,37 +68,37 @@ void *client( void *arg )
     periodicPacket.Encode( messageFrame );
     messageFrame.Serialize( messageBuffer );
 
-    OS_wait_us(5000000);
+    OS::waitUs(5000000);
 
-    OS_display("[CLIENT] Controller client has been started! ");
+    OS::display("[CLIENT] Controller client has been started! ");
 
     const uint16_t port_number_u16     = 5001;
 
-    OS_socket_address_in_t *p_server_socket_address_in_st;
-    OS_host_t              *p_host_st           = (OS_host_t*)(gethostbyname("127.0.0.1"));
-    int                     socket_int          = OS_socket_tcp();
+    Socket::address_in_t*   p_server_socket_address_in_st;
+    Socket::host_t*         p_host_st             = (Socket::host_t*)(gethostbyname("127.0.0.1"));
+    int                     socket_int            = Socket::_socketStream();
 
-    OS_display("[CLIENT] Client socket is created!");
+    OS::display("[CLIENT] Client socket is created!");
 
     if( p_host_st == NULL )
     {
-        OS_display("[CLIENT] No such host!");
+        OS::display("[CLIENT] No such host!");
     }
 
-    p_server_socket_address_in_st = OS_socket_address_in_init_by_name( p_host_st,  port_number_u16 );
+    p_server_socket_address_in_st = Socket::address_in_init_by_name( p_host_st,  port_number_u16 );
 
-    OS_display("[CLIENT] Client socket is initialized!");
+    OS::display("[CLIENT] Client socket is initialized!");
 
-    if( OS_connect( socket_int, p_server_socket_address_in_st) < 0 )
+    if( Socket::_connect( socket_int, p_server_socket_address_in_st) < 0 )
     {
-        OS_display("[CLIENT] Connection error!");
+        OS::display("[CLIENT] Connection error!");
     }
 
     while( true )
     {
-        OS_write(socket_int, messageBuffer.GetBuffer(), Needmon::FRAME_SIZE );
+        Socket::_write(socket_int, messageBuffer.GetBuffer(), Needmon::FRAME_SIZE );
 
-        OS_wait_us(1000000);
+        OS::waitUs(1000000);
        
     }
     
@@ -105,28 +107,28 @@ void *client( void *arg )
 
 int main()
 {
-    OS_display("[PROCESS] Controller has been started!");
+    OS::display("[PROCESS] Controller has been started!");
 
     /**
      * @brief 
      * 
      */
-    OS_thread_t thread_server;
-    OS_thread_t thread_client;
+    OS::thread_t thread_server;
+    OS::thread_t thread_client;
 
     /**
      * @brief Construct a new os thread create object
      * 
      */
-    OS_thread_create( &thread_server, server );
-    OS_thread_create( &thread_client, client );
+    OS::create( &thread_server, server );
+    OS::create( &thread_client, client );
 
     /**
      * @brief Construct a new os thread join object
      * 
      */
-    OS_thread_join( thread_server );
-    OS_thread_join( thread_client );
+    OS::join( thread_server );
+    OS::join( thread_client );
   
     return 0;
 }
